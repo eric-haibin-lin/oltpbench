46841393ab974c13d0816e2b7053a166e3f1624c
Haibin Lin <haibinl@node-logging.node-logging.cmu15721.narwhal.pdl.cmu.edu>
2016-04-19 13:35:27 -0600

    Update configuration for logging;

--- src/com/oltpbenchmark/distributions/ZipfianGenerator.java
+++ src/com/oltpbenchmark/distributions/ZipfianGenerator.java
@@ -54,7 +54,7 @@ import java.util.Random;
  */
 public class ZipfianGenerator extends IntegerGenerator
 {     
-	public static final double ZIPFIAN_CONSTANT=0.99;
+	public static final double ZIPFIAN_CONSTANT=0.;
 
 	/**
 	 * Number of items.
